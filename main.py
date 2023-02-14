3c42342a0c609601633145d54f9dd5a071c56124
tsubasa652 <34929737+tsubasa652@users.noreply.github.com>
2023-02-14 11:28:02 +0900

    update port

--- main.py
+++ main.py
@@ -33,4 +33,4 @@ async def root(item: Item):
         return {"status": "Error", "msg": "Unknown Error"}
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8082)
+    uvicorn.run(app, host="0.0.0.0", port=8080)
